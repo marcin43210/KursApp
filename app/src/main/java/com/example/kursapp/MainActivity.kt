e51b985c5ac34b714394ef0d9913bf9e5f0c01cc
Marcin <mmamelka@gmail.com>
2020-03-24 00:02:47 +0100

    odcinek 4 - intencje

--- app/src/main/java/com/example/kursapp/MainActivity.kt
+++ app/src/main/java/com/example/kursapp/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.kursapp
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button_1.setOnClickListener{
+            var message = Toast.makeText(applicationContext, "wiadomosc", Toast.LENGTH_LONG);
+            message.show();
+        }
 
-            button_1.setText("Kliknięty")
+        move_button.setOnClickListener{
+            var message = Toast.makeText(applicationContext, "krotka", Toast.LENGTH_SHORT);
+            message.show();
+
         }
 
 
