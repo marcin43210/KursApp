82e77802850e6c6632ad594f4721d885f8f45458
Marcin <mmamelka@gmail.com>
2020-03-29 00:34:06 +0100

    odcinek 4 - intencje

--- app/src/main/java/com/example/kursapp/MainActivity.kt
+++ app/src/main/java/com/example/kursapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kursapp
 
 import android.app.Activity
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,11 +18,19 @@ class MainActivity : AppCompatActivity() {
         button_1.setOnClickListener{
             var message = Toast.makeText(applicationContext, "wiadomosc", Toast.LENGTH_LONG);
             message.show();
+
+            var newAktywnosc: Intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(newAktywnosc)
+
         }
 
         move_button.setOnClickListener{
             var message = Toast.makeText(applicationContext, "krotka", Toast.LENGTH_SHORT);
             message.show();
+
+            val address = "https://www.youtube.com/channel/UCZHHfVxOTL40lqR_Kh3fg9A"
+            var channelYt = Intent(ACTION_VIEW, Uri.parse(address))
+            startActivity(channelYt)
 
         }
 
