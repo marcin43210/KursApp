f5f238962819ea14d7d83a377e6e408239eaf25e
Marcin <mmamelka@gmail.com>
2020-03-30 22:22:16 +0200

    hasla odc 6

--- app/src/main/java/com/example/kursapp/MainActivity.kt
+++ app/src/main/java/com/example/kursapp/MainActivity.kt
@@ -6,10 +6,13 @@ import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,4 +39,6 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
 }
--- app/src/main/java/com/example/kursapp/SecondActivity.kt
+++ app/src/main/java/com/example/kursapp/SecondActivity.kt
@@ -2,6 +2,8 @@ package com.example.kursapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -9,4 +11,18 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
     }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        if(password.isFocused){
+            if(password.length() <6 ){
+                warn_password.text = "Hasło za krótkie!"
+                warn_password.visibility = TextView.VISIBLE
+            }else{
+                warn_password.text = " Hasło ok!"
+                warn_password.visibility = TextView.VISIBLE
+            }
+        }
+    }
+
 }
